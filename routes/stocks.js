210d49d0e0f49447e8f6c36c3bf01b3350461af0
user100 <user100@example.com>
2023-02-18 15:51:39 +0200

    Added endpoint for health check

--- routes/stocks.js
+++ routes/stocks.js
@@ -19,11 +19,11 @@ const { protect, authorize } = require('../middleware/auth');
 const advancedResults = require('../middleware/advancedResults');
 
 router.route('/').get(advancedResults(Stock), getStocks);
+router.route('/health').get(getHealth)
 router.route('/:ticker').get(getStock).delete(protect, authorize('admin'), deleteStock)
 router.route('/:ticker/getdata').get(protect, getStockData)
 router.route('/:field/distinct').get(getDistinct)
 router.route('/:ticker/rename/:newticker').put(protect, authorize('admin'), updateStockTicker)
 router.route('/:ticker/removefindata/:type/:enddatey').put(protect, authorize('admin'), removeStockFinData)
-router.route('/health').get(getHealth)
 
 module.exports  = router;
